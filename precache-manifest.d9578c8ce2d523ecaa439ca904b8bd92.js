b447ad7d8fe77194cfce68d951e4ff83786c6c4d
btarman <btarman75@gmail.com>
2020-08-10 18:29:02 -0400

    Updates

---
+++ precache-manifest.d9578c8ce2d523ecaa439ca904b8bd92.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "720e7364a09e678dc4235fc8d445a1b3",
+    "url": "/github.io/Server-App/index.html"
+  },
+  {
+    "revision": "48c4c4e50309fc2f78e3",
+    "url": "/github.io/Server-App/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "0ad1660be6bcafe01a7f",
+    "url": "/github.io/Server-App/static/css/main.5c3a4c38.chunk.css"
+  },
+  {
+    "revision": "48c4c4e50309fc2f78e3",
+    "url": "/github.io/Server-App/static/js/2.53566d39.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/github.io/Server-App/static/js/2.53566d39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ad1660be6bcafe01a7f",
+    "url": "/github.io/Server-App/static/js/main.61ce3ab5.chunk.js"
+  },
+  {
+    "revision": "1d176479d7e280be2b4d",
+    "url": "/github.io/Server-App/static/js/runtime-main.cb8f3c6e.js"
+  },
+  {
+    "revision": "ac1839b05d92a3c9818f9063ad10098e",
+    "url": "/github.io/Server-App/static/media/45-degree-fabric-light.ac1839b0.png"
+  },
+  {
+    "revision": "218a7dd0ac1a4f297a766951b7410815",
+    "url": "/github.io/Server-App/static/media/IMG_0425.218a7dd0.jpg"
+  },
+  {
+    "revision": "f382d70dddb180d20b3b96c9372e6289",
+    "url": "/github.io/Server-App/static/media/Image-1.f382d70d.jpg"
+  },
+  {
+    "revision": "1fe109e32c48759ffcb92a360f2b7f87",
+    "url": "/github.io/Server-App/static/media/server_logo(inverse color).1fe109e3.jpg"
+  }
+]);
