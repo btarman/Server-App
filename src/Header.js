f384583c032dbd81d36202d935363256c59007ef
btarman <btarman75@gmail.com>
2020-08-10 20:41:20 -0400

    added script for route hosting

--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Navbar, Nav } from 'react-bootstrap';
-
+import { navLogo } from './images/logo_text_full-color_.png';
 function HeaderComponent(props) {
 	console.log(props);
 	return(
 		<Navbar collapseOnSelect expand="lg" className='navbar'>
 				
-  			<Navbar.Brand href="/"><img src="imgs/logo_text_full-color_.png" className='nav-logo' alt='nav-logo' /></Navbar.Brand>
+  			<Navbar.Brand href="/"><img src={ navLogo } className='nav-logo' alt='nav-logo' /></Navbar.Brand>
   			<Navbar.Toggle aria-controls="basic-navbar-nav" />
   			<Navbar.Collapse id="basic-navbar-nav">
     			<Nav className="mr-auto">
