9cbbef4e0fd1c343ee4df20e65a5e0defe6d0f0c
btarman <btarman75@gmail.com>
2020-08-18 17:52:04 -0400

    added node server file

---
+++ build/precache-manifest.63aa0bf9ad4a4429c8bfb18dcf14ef08.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "860b3602977f5a141e1d45fb902b14fc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "48c4c4e50309fc2f78e3",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "68acc4592e84fe4be081",
+    "url": "/static/css/main.5eee1a14.chunk.css"
+  },
+  {
+    "revision": "48c4c4e50309fc2f78e3",
+    "url": "/static/js/2.53566d39.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.53566d39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68acc4592e84fe4be081",
+    "url": "/static/js/main.98708b95.chunk.js"
+  },
+  {
+    "revision": "506809e0c884123d25f9",
+    "url": "/static/js/runtime-main.02206bb5.js"
+  },
+  {
+    "revision": "ac1839b05d92a3c9818f9063ad10098e",
+    "url": "/static/media/45-degree-fabric-light.ac1839b0.png"
+  },
+  {
+    "revision": "218a7dd0ac1a4f297a766951b7410815",
+    "url": "/static/media/IMG_0425.218a7dd0.jpg"
+  },
+  {
+    "revision": "f382d70dddb180d20b3b96c9372e6289",
+    "url": "/static/media/Image-1.f382d70d.jpg"
+  },
+  {
+    "revision": "1fe109e32c48759ffcb92a360f2b7f87",
+    "url": "/static/media/server_logo(inverse color).1fe109e3.jpg"
+  }
+]);
